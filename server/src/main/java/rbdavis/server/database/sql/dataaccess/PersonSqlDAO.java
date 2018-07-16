817b3164132ed7bcadbef85c149812e407b4b453
Tanner Davis <tannerdavisr@gmail.com>
2018-07-15 23:04:30 -0600

    Create functionality for PersonDAO
    Got boilerplate code for PersonDAO.create()

--- server/src/main/java/rbdavis/server/database/sql/dataaccess/PersonSqlDAO.java
+++ server/src/main/java/rbdavis/server/database/sql/dataaccess/PersonSqlDAO.java
@@ -1,10 +1,16 @@
 package rbdavis.server.database.sql.dataaccess;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import rbdavis.server.database.DAO;
 import rbdavis.server.database.sql.SqlConnectionManager;
+import rbdavis.shared.models.data.Gender;
 import rbdavis.shared.models.data.Person;
 
 /**
@@ -31,7 +37,52 @@ public class PersonSqlDAO implements DAO<Person> {
      */
     @Override
     public Person create(Person person) throws DatabaseException {
-        return null;
+        try {
+            connection = SqlConnectionManager.openConnection();
+            connection.setAutoCommit(false);
+            PreparedStatement stmt = null;
+            try {
+                String sql = "INSERT INTO Persons" +
+                        "(id, user_id, first_name, last_name, gender, father_id, mother_id, spouse_id)" +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                stmt = connection.prepareStatement(sql);
+                stmt.setString(1, person.getId());
+                stmt.setString(2, person.getUserId());
+                stmt.setString(3, person.getFirstName());
+                stmt.setString(4, person.getLastName());
+                stmt.setString(5, person.getGender().toString());
+                stmt.setString(6, person.getFatherId());
+                stmt.setString(7, person.getMotherID());
+                stmt.setString(8, person.getSpouseID());
+
+                if (stmt.executeUpdate() == 1) {
+                    connection.commit();
+                }
+                else {
+                    connection.rollback();
+                }
+            }
+            finally {
+                closeStatement(stmt);
+                SqlConnectionManager.closeConnection(connection);
+            }
+        }
+        catch (SQLException e) {
+            String errorMessage;
+            switch (e.getErrorCode()) {
+                case 1:
+                    errorMessage = "Invalid SQL syntax.";
+                    break;
+                case 19:
+                    errorMessage = "ID is already taken.";
+                    break;
+                default:
+                    errorMessage = "Create person failed.";
+                    break;
+            }
+            throw new DAO.DatabaseException(errorMessage, e);
+        }
+        return person;
     }
 
     /**
@@ -80,6 +131,55 @@ public class PersonSqlDAO implements DAO<Person> {
     @Override
     public List<Person> all() throws DatabaseException {
         return null;
+    }
+
+    private Person extractPersonModel(ResultSet rs) throws SQLException {
+        Person person = null;
+
+        String id = rs.getString(1);
+        String userId = rs.getString(2);
+        String firstName = rs.getString(3);
+        String lastName = rs.getString(4);
+        String genderStr = rs.getString(5);
+        String fatherId = rs.getString(6);
+        String motherId = rs.getString(7);
+        String spouseid = rs.getString(8);
+        Gender gender;
+        if (genderStr.equals("m")) {
+            gender = Gender.M;
+        }
+        else {
+            gender = Gender.F;
+        }
+
+        person = new Person(id, userId, firstName, lastName, gender, fatherId, motherId, spouseid);
+
+        return person;
+    }
+
+    private List<Person> extractPersonModels(ResultSet rs) throws SQLException {
+        List<Person> people = new ArrayList<>();
+
+        while (rs.next()) {
+            Person person = extractPersonModel(rs);
+            if (person != null) {
+                people.add(person);
+            }
+        }
+
+        return people;
+    }
+
+    private void closeStatement(Statement stmt) throws SQLException {
+        if (stmt != null) {
+            stmt.close();
+        }
+    }
+
+    private void closeResultSet(ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
     }
 
 }
