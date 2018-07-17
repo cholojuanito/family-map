b5bc4491a9df6e2a36012d79ff31ac6092d0c828
Tanner Davis <tannerdavisr@gmail.com>
2018-07-17 13:53:08 -0600

    Repeat
    It missed one apparently.

---
+++ server/src/main/java/rbdavis/server/handlers/RegisterHandler.java
@@ -0,0 +1,13 @@
+package rbdavis.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class RegisterHandler implements HttpHandler{
+
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
